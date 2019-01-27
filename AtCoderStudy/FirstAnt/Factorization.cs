dd5149084f58918c108e9c1666a053300cb3d126
ma3100 <masato.syuukatu@gmail.com>
2019-01-27 21:54:05 +0900

    Add Factorization

---
+++ AtCoderStudy/FirstAnt/Factorization.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoderStudy.FirstAnt
+{
+	public class Factorization
+	{
+		static long mod = 1000000007;
+
+		public void Do()
+		{
+			// スペース区切りの整数の入力
+			var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+			var N = input[0];
+			var M = input[1];
+
+			int[] indexes = index(M);
+			long ans = 1;
+			if (M != 1)
+			{
+				for (int i = 0; i < indexes.Count(); i++)
+				{
+					ans *= nHk(indexes[i] + N - 1, N - 1);
+				}
+			}
+			Console.WriteLine(ans % mod);
+		}
+
+		static int[] index(int n)
+		{
+			var list = new List<int>();
+			if (n == 1)
+			{
+				list.Add(1);
+			}
+			else
+			{
+				int i = 2;
+				int temp = 0;
+				while (n != 1)
+				{
+					if (n % i == 0)
+					{
+						temp++;
+						n /= i;
+					}
+					else
+					{
+						if (temp != 0)
+						{
+							list.Add(temp);
+						}
+						temp = 0;
+						i++;
+					}
+				}
+				list.Add(temp);
+			}
+			int[] res = list.ToArray();
+			return res;
+		}
+
+		static long nHk(int n, int r)
+		{
+			if (n < 0 || r < 0 || r > n) throw new ArgumentException("不正な引数です．");
+
+			if (n - r < r) r = n - r;
+			if (r == 0) return 1;
+			if (r == 1) return n;
+
+			int[] numerator = new int[r];
+			int[] denominator = new int[r];
+
+			for (int k = 0; k < r; k++)
+			{
+				numerator[k] = n - r + k + 1;
+				denominator[k] = k + 1;
+			}
+
+			for (int p = 2; p <= r; p++)
+			{
+				int pivot = denominator[p - 1];
+				if (pivot > 1)
+				{
+					int offset = (n - r) % p;
+					for (int k = p - 1; k < r; k += p)
+					{
+						numerator[k - offset] /= pivot;
+						denominator[k] /= pivot;
+					}
+				}
+			}
+			long result = 1;
+			for (int k = 0; k < r; k++)
+			{
+				if (numerator[k] > 1) result *= numerator[k];
+			}
+
+			return result;
+		}
+	}
+}
