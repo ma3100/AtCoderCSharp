858ee41d05b379347ca2b4e451aa287a5085c3a8
ma3100 <masato.syuukatu@gmail.com>
2019-01-31 08:34:12 +0900

    add iwi

---
+++ AtCoderStudy/FirstAnt/iwi.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoderStudy.FirstAnt
+{
+	public class Iwi
+	{
+		public void Do()
+		{
+			// 文字列の入力
+			var input = Console.ReadLine();
+			var resultList = new List<int>();
+			for (int i = 0; i < input.Length; i++)
+			{
+				// 文字が三文字以上残っているなら
+				if (i + 3 <= input.Length)
+				{
+					var targetValue = input.Substring(i, 3);
+					if (targetValue == "iwi")
+					{
+						var before = input.Substring(0, i);
+						var after = input.Substring(i + 3);
+
+						resultList.Add(resolve(before + after, 1));
+					}
+				}
+			}
+
+			Console.WriteLine(resultList.Max());
+		}
+
+		public int resolve(string value,int count)
+		{
+			for (int i = 0; i < value.Length; i++)
+			{
+				// 文字が三文字以上残っているなら
+				if (i + 3 <= value.Length)
+				{
+					var targetValue = value.Substring(i, 3);
+					if (targetValue == "iwi")
+					{
+						var before = value.Substring(0, i);
+						var after = value.Substring(i + 3);
+
+						resolve(before + after, ++count);
+					}
+				}
+			}
+
+			return count;
+		}
+	}
+}
--- AtCoderStudy/Program.cs
+++ AtCoderStudy/Program.cs
@@ -4,108 +4,49 @@ using System.Linq;
 
 class Program
 {
-
-	static char[,] A = new char[5, 5]; // 盤面の情報
-	static int[,] b = new int[5, 5];// 得点の情報1
-	static int[,] c = new int[5, 5]; // 得点の情報2
-
 	public static void Main(string[] args)
 	{
-		// 入力
-		int sum = 0; // 全体の合計
-		for (int i = 0; i < 2; i++)
+		// 文字列の入力
+		var input = Console.ReadLine();
+		var resultList = new List<int>();
+		for (int i = 0; i < input.Length; i++)
 		{
-			// スペース区切りの整数の入力
-			var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-			for (int j = 0; j < 3; j++)
+			// 文字が三文字以上残っているなら
+			if (i + 3 <= input.Length)
 			{
-				b[i, j] = input[j];
-				sum += b[i, j];
-			}
-		}
-		for (int i = 0; i < 3; i++)
-		{
-			// スペース区切りの整数の入力
-			var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-			for (int j = 0; j < 2; j++)
-			{
-				c[i, j] = input[j];
-				sum += c[i, j];
-			}
-		}
-
-		// 盤面を全て'*'で初期化
-		for (int x = 0; x < 5; x++)
-		{
-			for (int y = 0; y < 5; y++)
-			{
-				A[x, y] = '*';
-			}
-		}
-
-		int answer = dfs(0); // 直大の得点が返ってくる
-		Console.WriteLine(answer);
-		Console.WriteLine(sum - answer);
-	}
-
-	// 盤面を全通りに埋めるDFS
-	static int dfs(int turn)
-	{
-		if (turn == 9) return calc();
-
-		if (turn % 2 == 0)
-		{
-			int Max = -1;
-			for (int i = 0; i < 3; i++)
-			{
-				for (int j = 0; j < 3; j++)
+				var targetValue = input.Substring(i, 3);
+				if (targetValue == "iwi")
 				{
-					if (A[i, j] != '*') continue;
-					A[i, j] = 'o'; // 盤面に'o'を打つ
-					Max = Math.Max(Max, dfs(turn + 1));
-					A[i, j] = '*'; // 盤面を元に戻す
+					var before = input.Substring(0, i);
+					var after = input.Substring(i + 3);
+
+					resultList.Add(resolve(before + after, 1));
 				}
 			}
-			return Max;
 		}
-		else
-		{
-			int Min = 1;
-			for (int i = 0; i < 3; i++)
-			{
-				for (int j = 0; j < 3; j++)
-				{
-					if (A[i, j] != '*') continue;
-					A[i, j] = 'x'; // 盤面に'x'を打つ
-					Min = Math.Min(Min, dfs(turn + 1));
-					A[i, j] = '*'; // 盤面を元に戻す
-				}
-			}
-			return Min;
-		}
+
+		Console.WriteLine(resultList.Max());
 	}
 
-	// 盤面を埋め終わったとき、直大の得点を返す
-	static int calc()
+	public static int resolve(string value, int count)
 	{
-		int score = 0;
-		// b[2][3]についての直大の得点
-		for (int i = 0; i < 2; i++)
+		for (int i = 0; i < value.Length; i++)
 		{
-			for (int j = 0; j < 3; j++)
+			// 文字が三文字以上残っているなら
+			if (i + 3 <= value.Length)
 			{
-				if (A[i, j] == A[i + 1, j]) score += b[i, j];
+				var targetValue = value.Substring(i, 3);
+				if (targetValue == "iwi")
+				{
+					var before = value.Substring(0, i);
+					var after = value.Substring(i+3);
+
+					resolve(before + after, ++count);
+				}
 			}
 		}
-		// c[3][2]についての直大の得点
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 2; j++)
-			{
-				if (A[i, j] == A[i, j + 1]) score += c[i, j];
-			}
-		}
-		return score;
+
+		return count;
 	}
 }
 
