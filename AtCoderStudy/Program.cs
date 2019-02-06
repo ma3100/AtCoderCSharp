cd388eb4ac5933e4f6e224fb4419810d31dd6b94
ma3100 <masato.syuukatu@gmail.com>
2019-02-06 12:11:48 +0900

    Another Answer

--- AtCoderStudy/Program.cs
+++ AtCoderStudy/Program.cs
@@ -10,13 +10,21 @@ class Program
 		long[] value = new long[100];
 
 		// DPテーブル
-		long[,] dp = new long[100, 10000];
-
+		long[,] dp = new long[10, 10];
 
 		// スペース区切りの整数の入力
 		var inputCondition = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
 		var N = inputCondition[0];
 		var W = inputCondition[1];
+
+		// dpを全て大きな値で初期化
+		for (int i = 0; i < 10; ++i)
+		{
+			for (int j = 0; j < 10; ++j)
+			{
+				dp[i, j] = 1000000000;
+			}
+		}
 
 		for (int i = 0; i < N; i++)
 		{
@@ -26,6 +34,9 @@ class Program
 			value[i] = inputWeightAndValue[1];
 		}
 
+		// 初期条件
+		dp[0, 0] = 0;
+
 		// DPループ
 		for (int i = 0; i < N; ++i)
 		{
@@ -34,20 +45,19 @@ class Program
 				// i 番目の品物を選ぶ場合
 				if (sum_w - weight[i] >= 0)
 				{
-					if (dp[i + 1, sum_w] < (dp[i, sum_w - weight[i]] + value[i]))
+					if (dp[i + 1, sum_w] > (dp[i, sum_w - weight[i]] + value[i]))
 					{
 						dp[i + 1, sum_w] = dp[i, sum_w - weight[i]] + value[i];
 					}
 				}
 
 				// i 番目の品物を選ばない場合
-				if (dp[i + 1, sum_w] < (dp[i, sum_w]))
+				if (dp[i + 1, sum_w] > (dp[i, sum_w]))
 				{
 					dp[i + 1, sum_w] = (dp[i, sum_w]);
 				}
 			}
 		}
-
 		Console.WriteLine(dp[N, W]);
 	}
 
