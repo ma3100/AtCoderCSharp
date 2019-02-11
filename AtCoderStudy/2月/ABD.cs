3a496c48548b868f7514fbe4659b5b4903fe97cc
ma3100 <masato.syuukatu@gmail.com>
2019-02-11 19:35:42 +0900

    ABD Add

---
+++ AtCoderStudy/2月/ABD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoderStudy._2月
+{
+	public class ABD
+	{
+		public void Do()
+		{
+			var N = int.Parse(Console.ReadLine());
+			if (N >= 1000)
+				Console.WriteLine("ABD");
+			else
+				Console.WriteLine("ABC");
+		}
+	}
+}
--- AtCoderStudy/Program.cs
+++ AtCoderStudy/Program.cs
@@ -4,52 +4,13 @@ using System.Linq;
 
 class Program
 {
-	static int[] h;
-	static List<int>[] li;
-
 	public static void Main(String[] args)
 	{
-		// スペース区切りの整数の入力
-		var inputCondition = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-
-		var n = inputCondition[0];
-		var m = inputCondition[1];
-
-		h = new int[n + 1];
-		li = new List<int>[n + 1];
-		// 初期化
-		for (int i = 1; i <= n; i++)
-		{
-			li[i] = new List<int>();
-			h[i] = -1;
-		}
-
-		// 繋がっているノードを取得
-		for (int i = 0; i < m; i++)
-		{
-			int[] q = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-			li[q[0]].Add(q[1]);
-		}
-		int ans = 0;
-		for (int i = 1; i <= n; i++)
-		{
-			ans = Math.Max(fu(i), ans);
-		}
-		Console.WriteLine(ans);
-	}
-	static int fu(int a)
-	{
-		if (h[a] != -1) { return h[a]; }
-		int p = 0;
-		for (int i = 0; i < li[a].Count; i++)
-		{
-			if (h[li[a][i]] == -1)
-			{
-				h[li[a][i]] = fu(li[a][i]);
-			}
-			p = Math.Max(h[li[a][i]] + 1, p);
-		}
-		return p;
+		var N = int.Parse(Console.ReadLine());
+		if (N >= 1000)
+			Console.WriteLine("ABD");
+		else
+			Console.WriteLine("ABC");
 	}
 
 }
