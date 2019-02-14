fc17835bc178cb404ef3934a8bb45049e9c07cb9
ma3100 <masato.syuukatu@gmail.com>
2019-02-14 19:55:21 +0900

    Add Triangle

---
+++ AtCoderStudy/2月/RightTriangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoderStudy._2月
+{
+	public class RightTriangle
+	{
+		public void Do()
+		{
+			// スペース区切りの整数の入力
+			var input = Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(x => x).ToArray();
+			Console.WriteLine((input[0] * input[1]) / 2);
+
+		}
+	}
+}
--- AtCoderStudy/Program.cs
+++ AtCoderStudy/Program.cs
@@ -7,40 +7,8 @@ class Program
 	public static void Main(String[] args)
 	{
 		// スペース区切りの整数の入力
-		var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-
-		var flag = true;
-		var count = 2;
-		var answer = 0;
-		var targetIndex = 0;
-		while (flag)
-		{
-			var treeSize = GetTreeSize(count);
-			if (treeSize > input[targetIndex])
-			{
-				answer = treeSize - input[targetIndex];
-				targetIndex++;
-				// 次にいけない時。
-				if(targetIndex == 2)
-					flag = false;
-			}
-			else
-			{
-				targetIndex = 0;
-				answer = 0;
-			}
-			count++;
-		}
-
-		Console.WriteLine(answer);
-	}
-
-	private static int GetTreeSize(int value)
-	{
-		if (value == 1)
-			return value;
-
-		return (GetTreeSize(value - 1)) + value;
+		var input = Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(x => x).ToArray();
+		Console.WriteLine((input[0] * input[1]) / 2);
 	}
 }
 
