5891a05c08a404177fd5b1b5ce2f44c7ee0b093b
ma3100 <masato.syuukatu@gmail.com>
2019-02-19 20:29:16 +0900

    Add Anti Adjacency

---
+++ AtCoderStudy/2月/AntiAdjacency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoderStudy._2月
+{
+	public class AntiAdjacency
+	{
+		public void Do()
+		{
+			// スペース区切りの整数の入力
+			var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+			var N = input[0];
+			var K = input[1];
+
+			if (N >= (2 * K - 1))
+				Console.WriteLine("YES");
+			else
+				Console.WriteLine("NO");
+		}
+	}
+}
