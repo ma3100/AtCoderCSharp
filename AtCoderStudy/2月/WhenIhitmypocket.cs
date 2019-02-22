019ee2000f5e131416429f009b895924639ca122
ma3100 <masato.syuukatu@gmail.com>
2019-02-22 21:02:08 +0900

    Add Vest Do

--- AtCoderStudy/2月/WhenIhitmypocket.cs
+++ AtCoderStudy/2月/WhenIhitmypocket.cs
@@ -8,16 +8,38 @@ namespace AtCoderStudy._2月
 {
 	public class WhenIhitmypocket
 	{
-		public void NextDo()
+		public void VestDo()
 		{
-			// スペース区切りの整数の入力
-			var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+			int[] input = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
 			var K = input[0];
 			var A = input[1];
 			var B = input[2];
 
-			var money = 0;
-			var bisketNum = 1;
+			long n = 0;
+			if (B > A + 2 && K >= A + 1)
+			{
+				n = Math.Min(A, K + 1);
+				int z = K - A + 1;
+				n += (long)(z / 2) * (B - A);
+				if (z % 2 == 1) n++;
+			}
+			else
+			{
+				n = K + 1;
+			}
+			Console.WriteLine(n);
+		}
+
+		public void NextDo()
+		{
+			// スペース区切りの整数の入力
+			var input = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+			long K = input[0];
+			long A = input[1];
+			long B = input[2];
+
+			long money = 0;
+			long bisketNum = 1;
 			if (A + 1 >= B)
 				bisketNum += K;
 			else
@@ -28,9 +50,11 @@ namespace AtCoderStudy._2月
 
 				bisketNum = changeNum * B + other;
 			}
+
+			Console.WriteLine(bisketNum);
 		}
 
-			public void Do()
+		public void Do()
 		{
 			// スペース区切りの整数の入力
 			var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
