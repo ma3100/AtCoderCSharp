b365c7b7eaa0921ee1940eae38a19ef9714983d2
ma3100 <masato.syuukatu@gmail.com>
2019-02-24 21:40:19 +0900

    Add OddSubrectangle

---
+++ AtCoderStudy/2月/OddSubrectangles.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Math;
+namespace AtCoderStudy._2月
+{
+	public class OddSubrectangles
+	{
+		int F => int.Parse(Str);
+		long FL => long.Parse(Str);
+		int[] G => Strs.Select(int.Parse).ToArray();
+		long[] GL => Strs.Select(long.Parse).ToArray();
+		string Str => ReadLine();
+		string[] Strs => Str.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+		const int MOD = 998244353;
+
+		public OddSubrectangles()
+		{
+			SetOut(new StreamWriter(OpenStandardOutput()) { AutoFlush = false });
+			WriteLine(Solve());
+			Out.Flush();
+		}
+
+		Random rand = new Random();
+		long Solve()
+		{
+			var I = G;
+			int Y = I[0], X = I[1];
+			var mat = new bool[Y][];
+			for (var y = 0; y < Y; y++)
+			{
+				mat[y] = new bool[X];
+				I = G;
+				for (var x = 0; x < X; x++) mat[y][x] = I[x] != 0;
+			}
+			var r = 0;
+			for (var c = 0; c < X; c++)
+			{
+				int y = r;
+				while (y < Y && !mat[y][c]) y++;
+				if (y == Y) continue;
+				if (y != r) AddTo(mat[r], mat[y]);
+				for (y = r + 1; y < Y; y++) if (mat[y][c]) AddTo(mat[y], mat[r]);
+				r++;
+			}
+			return (Pow(2, X + Y - 1, MOD) - Pow(2, X + Y - r - 1, MOD) + MOD) % MOD;
+		}
+		void Print(bool[] a) => WriteLine(string.Join("", a.Select(b => b ? 1 : 0)));
+		public static long Pow(long a, long b, long mod)
+		{
+			var p = 1L;
+			var x = a;
+			while (b > 0)
+			{
+				if ((b & 1) == 1) p = (p * x) % mod;
+				b >>= 1;
+				x = (x * x) % mod;
+			}
+			return p;
+		}
+		// a += b
+		void AddTo(bool[] a, bool[] b)
+		{
+			var n = a.Length;
+			for (var i = 0; i < n; i++) a[i] = a[i] ^ b[i];
+		}
+
+	}
+}
--- AtCoderStudy/Program.cs
+++ AtCoderStudy/Program.cs
@@ -1,53 +1,70 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using BI = System.Numerics.BigInteger;
-
-class Program
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Math;
+class Z { static void Main() => new K(); }
+class K
 {
-	public static void Main(String[] args)
+	int F => int.Parse(Str);
+	long FL => long.Parse(Str);
+	int[] G => Strs.Select(int.Parse).ToArray();
+	long[] GL => Strs.Select(long.Parse).ToArray();
+	string Str => ReadLine();
+	string[] Strs => Str.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+	const int MOD = 998244353;
+	public K()
 	{
-		var L = Int32.Parse(Console.ReadLine());
-		var A = new List<long>();
-		for (int i = 0; i < L; ++i)
-		{
-			A.Add(Int64.Parse(Console.ReadLine()));
-		}
-		var dp = new long[L, 5];
-		for (int i = 0; i < L; ++i)
-		{
-			var num = A[i] % 2;
-			var even = A[i] == 0 ? 2 : num == 0 ? 0 : 1;
-			var odd = A[i] == 0 ? 1 : num == 0 ? 1 : 0;
-			var value = GetValue(dp, i, 0);
-			dp[i, 0] = A[i] + value;
-			value = GetValue(dp, i, 1, value);
-			dp[i, 1] = even + value;
-			value = GetValue(dp, i, 2, value);
-			dp[i, 2] = odd + value;
-			value = GetValue(dp, i, 3, value);
-			dp[i, 3] = even + value;
-			value = GetValue(dp, i, 4, value);
-			dp[i, 4] = A[i] + value;
-		}
-		var ans = Math.Min(dp[L - 1, 0],
-				  Math.Min(dp[L - 1, 1],
-				  Math.Min(dp[L - 1, 2],
-				  Math.Min(dp[L - 1, 3], dp[L - 1, 4]))));
-		Console.WriteLine(ans);
+		SetOut(new StreamWriter(OpenStandardOutput()) { AutoFlush = false });
+		WriteLine(Solve());
+		Out.Flush();
 	}
-	static long GetValue(long[,] dp, long num, int section, long value = long.MaxValue)
+	Random rand = new Random();
+	long Solve()
 	{
-		if (num == 0) return 0;
-		else
+		var I = G;
+		int Y = I[0], X = I[1];
+		var mat = new bool[Y][];
+		for (var y = 0; y < Y; y++)
 		{
-			switch (section)
-			{
-				case 0: return dp[num - 1, section];
-				default: return Math.Min(value, dp[num - 1, section]);
-			}
+			mat[y] = new bool[X];
+			I = G;
+			for (var x = 0; x < X; x++) mat[y][x] = I[x] != 0;
 		}
+		var r = 0;
+		for (var c = 0; c < X; c++)
+		{
+			int y = r;
+			while (y < Y && !mat[y][c]) y++;
+			if (y == Y) continue;
+			if (y != r) AddTo(mat[r], mat[y]);
+			for (y = r + 1; y < Y; y++) if (mat[y][c]) AddTo(mat[y], mat[r]);
+			r++;
+		}
+		return (Pow(2, X + Y - 1, MOD) - Pow(2, X + Y - r - 1, MOD) + MOD) % MOD;
+	}
+	void Print(bool[] a) => WriteLine(string.Join("", a.Select(b => b ? 1 : 0)));
+	public static long Pow(long a, long b, long mod)
+	{
+		var p = 1L;
+		var x = a;
+		while (b > 0)
+		{
+			if ((b & 1) == 1) p = (p * x) % mod;
+			b >>= 1;
+			x = (x * x) % mod;
+		}
+		return p;
+	}
+	// a += b
+	void AddTo(bool[] a, bool[] b)
+	{
+		var n = a.Length;
+		for (var i = 0; i < n; i++) a[i] = a[i] ^ b[i];
 	}
 }
-
-
