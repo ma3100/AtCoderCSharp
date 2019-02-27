b1356ecbcd8250687bb5c615c8cbd1e1a50271e8
ma3100 <masato.syuukatu@gmail.com>
2019-02-27 19:26:25 +0900

    Add easy problem

---
+++ AtCoderStudy/2月/DigitalGifts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoderStudy._2月
+{
+	public class DigitalGifts
+	{
+		public void Do()
+		{
+			var N = int.Parse(Console.ReadLine());
+			double total = 0;
+
+			for (int i = 0; i < N; i++)
+			{
+				// スペース区切りの整数の入力
+				var input = Console.ReadLine().Split(' ');
+				if (input[1] == "JPY")
+				{
+					total += double.Parse(input[0]);
+				}
+				else
+				{
+					total += double.Parse(input[0]) * 380000;
+				}
+			}
+			Console.WriteLine(total);
+		}
+	}
+}
--- AtCoderStudy/Program.cs
+++ AtCoderStudy/Program.cs
@@ -1,70 +1,25 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Console;
-using static System.Math;
-class Z { static void Main() => new K(); }
-class K
+public class Program
 {
-	int F => int.Parse(Str);
-	long FL => long.Parse(Str);
-	int[] G => Strs.Select(int.Parse).ToArray();
-	long[] GL => Strs.Select(long.Parse).ToArray();
-	string Str => ReadLine();
-	string[] Strs => Str.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-	const int MOD = 998244353;
-	public K()
+	public static void Main(string[] args)
 	{
-		SetOut(new StreamWriter(OpenStandardOutput()) { AutoFlush = false });
-		WriteLine(Solve());
-		Out.Flush();
-	}
-	Random rand = new Random();
-	long Solve()
-	{
-		var I = G;
-		int Y = I[0], X = I[1];
-		var mat = new bool[Y][];
-		for (var y = 0; y < Y; y++)
+		var N = int.Parse(Console.ReadLine());
+		double total = 0;
+
+		for (int i = 0; i < N; i++)
 		{
-			mat[y] = new bool[X];
-			I = G;
-			for (var x = 0; x < X; x++) mat[y][x] = I[x] != 0;
+			// スペース区切りの整数の入力
+			var input = Console.ReadLine().Split(' ');
+			if (input[1] == "JPY")
+			{
+				total += double.Parse(input[0]);
+			}
+			else
+			{
+				total += double.Parse(input[0]) * 380000;
+			}
 		}
-		var r = 0;
-		for (var c = 0; c < X; c++)
-		{
-			int y = r;
-			while (y < Y && !mat[y][c]) y++;
-			if (y == Y) continue;
-			if (y != r) AddTo(mat[r], mat[y]);
-			for (y = r + 1; y < Y; y++) if (mat[y][c]) AddTo(mat[y], mat[r]);
-			r++;
-		}
-		return (Pow(2, X + Y - 1, MOD) - Pow(2, X + Y - r - 1, MOD) + MOD) % MOD;
-	}
-	void Print(bool[] a) => WriteLine(string.Join("", a.Select(b => b ? 1 : 0)));
-	public static long Pow(long a, long b, long mod)
-	{
-		var p = 1L;
-		var x = a;
-		while (b > 0)
-		{
-			if ((b & 1) == 1) p = (p * x) % mod;
-			b >>= 1;
-			x = (x * x) % mod;
-		}
-		return p;
-	}
-	// a += b
-	void AddTo(bool[] a, bool[] b)
-	{
-		var n = a.Length;
-		for (var i = 0; i < n; i++) a[i] = a[i] ^ b[i];
+		Console.WriteLine(total);
+
 	}
 }
