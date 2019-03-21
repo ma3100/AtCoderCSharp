51e014a79f3a543043e84d7e4aa7e4d96d50b3a3
ma3100 <masato.syuukatu@gmail.com>
2019-03-21 18:40:57 +0900

    Add B

---
+++ AtCoderStudy/3月/Reversi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoderStudy._3月
+{
+	public static class Reversi
+	{
+		const int MOD = 1000000007;
+
+		public static void Solve()
+		{
+			int N;
+			long res = 1;
+			int[] tmp = new int[200010]; // 各色の一時的なDP値の格納場所
+			N = int.Parse(Console.ReadLine());
+			int[] array = new int[N + 1];
+			for (int i = 1; i <= N; i++)
+			{
+				array[i] = int.Parse(Console.ReadLine());
+			}
+			int[] dp = new int[N + 1];
+			dp[0] = dp[1] = 1; // 最初の石までのDP値は1
+			tmp[array[1]] = 1;
+
+			for (int i = 2; i <= N; i++)
+			{
+				if (array[i] == array[i - 1])
+				{
+					dp[i] = dp[i - 1];
+				}
+				else
+				{
+					tmp[array[i]] += dp[i - 1];
+					tmp[array[i]] %= MOD;
+					dp[i] = tmp[array[i]];
+				}
+			}
+			res = dp[N];
+
+			Console.WriteLine(res);
+
+		}
+	}
+}
--- AtCoderStudy/Program.cs
+++ AtCoderStudy/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AtCoderStudy._3月;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,30 +8,8 @@ public class Program
 
 	static void Main(string[] args)
 	{
-		Console.WriteLine(Solve());
+		Reversi.Solve();
 		return;
 	}
 
-	public static string Solve()
-	{
-		long MOD = 1000000007;
-		long ans = 0;
-
-		int N = int.Parse(Console.ReadLine());
-		string S = Console.ReadLine();
-
-		int[] cnt = new int[27];
-		for (int i = 0; i < N; i++)
-		{
-			cnt[S[i] - 'a']++;
-		}
-		//Console.WriteLine(string.Join(", ",cnt));
-		ans = 1;
-		for (int i = 0; i < 27; i++)
-		{
-			ans = (ans * (cnt[i] + 1)) % MOD;
-		}
-		ans--;
-		return ans.ToString();
-	}
 }
