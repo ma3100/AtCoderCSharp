96a057c4a036ea3d3a0a41ea51e88e0be3692a29
ma3100 <masato.syuukatu@gmail.com>
2019-03-25 00:29:37 +0900

    AtCoder

---
+++ AtCoderStudy/3月/ATCoder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoderStudy._3月
+{
+	public class ATCoder
+	{
+		public void Do()
+		{
+			// 文字の入力
+			var S = Console.ReadLine();
+			var answerList = new List<char>() { 'A', 'C', 'G', 'T' };
+			var count = 0;
+			var maxCount = 0;
+			foreach (var c in S)
+			{
+				if(answerList.Any(x => x == c))
+				{
+					count++;
+				}
+				else
+				{
+					if(maxCount < count)
+					{
+						maxCount = count;
+					}
+					count = 0;
+				}
+			}
+
+			if (maxCount < count)
+			{
+				maxCount = count;
+			}
+
+			Console.WriteLine(maxCount);
+		}
+	}
+}
--- AtCoderStudy/Program.cs
+++ AtCoderStudy/Program.cs
@@ -1,5 +1,4 @@
-﻿using AtCoderStudy._3月;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,8 +7,33 @@ public class Program
 
 	static void Main(string[] args)
 	{
-		Reversi.Solve();
-		return;
+		// 文字の入力
+		var S = Console.ReadLine();
+		var answerList = new List<char>() { 'A', 'C', 'G', 'T' };
+		var count = 0;
+		var maxCount = 0;
+		foreach (var c in S)
+		{
+			if (answerList.Any(x => x == c))
+			{
+				count++;
+			}
+			else
+			{
+				if (maxCount < count)
+				{
+					maxCount = count;
+				}
+				count = 0;
+			}
+		}
+
+		if (maxCount < count)
+		{
+			maxCount = count;
+		}
+
+		Console.WriteLine(maxCount);
 	}
 
 }
